2e97eb8a4fef3ebdaafce954833adeea2d05788f
mrkeshav.05 <keshavpundhir69@gmail.com>
2024-06-11 12:25:38 +0530

    7

--- chat_app_frontend/src/components/sidebar/Conversations.jsx
+++ chat_app_frontend/src/components/sidebar/Conversations.jsx
@@ -8,11 +8,11 @@ const Conversations = () => {
   console.log(getRandomEmoji())
   return (
     <div className='py-2 flex-1 flex-col overflow-auto'>
-      {
+      {/* {
         Array.isArray(conversations) && !loading && (
           <p className='text-center text-2xl text-gray-400'>Koi nhi hai <br /> baat karne ko 🥱😴</p>
         )
-      }
+      } */}
       {loading ? (
         <span className='loading loading-spinner mx-auto'></span>
       ) : (
